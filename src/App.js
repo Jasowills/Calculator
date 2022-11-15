6b5f316d23783cfe756bc74ba874c3082b792d57
Jasowills <Jujuzhaddy01@gmail.com>
2022-11-15 17:11:18 +0100

    origin

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Calculator from "./components/Calculator";
+import Atrribute from "./Atrribute";
+
+function App() {
+  return (
+    <>
+      <Calculator />
+      <Atrribute />;
+    </>
+    
+  )
+ 
+}
+
+export default App;
---
+++ src/Atrribute.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Atrribute = () => {
+  return (
+    <div>
+      <h3 className={'attribute'}>Created by <a href="https://github.com/Jasowills">Jasowills 👌</a></h3>
+    </div>
+  )
+}
+
+export default Atrribute
